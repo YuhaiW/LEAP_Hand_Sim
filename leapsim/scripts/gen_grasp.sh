a04bf6a99f06389b6a07c0632244f74c06b414d9
YuhaiW <yuhaiwan@usc.edu>
2024-09-01 20:54:15 -0700

    sep 1

--- leapsim/scripts/gen_grasp.sh
+++ leapsim/scripts/gen_grasp.sh
@@ -16,6 +16,6 @@ len=${#array[@]}
 EXTRA_ARGS=${array[@]:2:$len}
 EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
 
-python3 train.py task=LeapHandGrasp task.env.baseObjScale=$SCALE \
+python3 train.py wandb_activate=false task=LeapHandGrasp task.env.baseObjScale=$SCALE \
 task.env.grasp_cache_name=$GRASP_CACHE test=true pipeline=cpu test=true train.params.config.player.games_num=10000000000000000 task.env.episodeLength=50 \
 ${EXTRA_ARGS}
